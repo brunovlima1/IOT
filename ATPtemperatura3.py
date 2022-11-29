f7b8269a64f8b38b4fb0c1cfa3c7ee37b615c341
Bruno Lima <116768735+brunovlima1@users.noreply.github.com>
2022-11-29 13:04:34 -0300

    Add files via upload

---
+++ ATPtemperatura3.py
@@ -0,0 +1,43 @@
+#Bruno Veiga de Lima
+import dht
+import machine
+import time
+import network
+import urequests
+from wifi_lib import conecta
+
+#constancias e variáveis
+station = conecta("CASA 98", "68788898");
+http_headers = {'content-Type': 'application/json'}
+thingspeak_api_write_key = '01YHDV5ZOBZF9WR8'
+update_time_interval = 5000
+last_update = time.ticks_ms()
+
+
+
+d = dht.DHT11(machine.Pin(4))
+r = machine.Pin(2,machine.Pin.OUT)
+
+#instaciação de temperatura e loop de envio de dados para o thingspeak
+while True:
+    d.measure()
+    temp = d.temperature()
+    humid = d.humidity()
+    if temp > 31 or humid > 70:
+      r.value(1)
+    else:
+        r.value(0)
+    print("Temperatura: {} Umidade: {}".format(d.temperature(), d.humidity()))
+    time.sleep(15)
+    
+    if time.ticks_ms() - last_update >= update_time_interval: 
+        
+         
+        dht_readings = {'field1':temp, 'field2':humid} 
+        request = urequests.post( 
+          'http://api.thingspeak.com/update?api_key=' +
+          thingspeak_api_write_key, 
+          json = dht_readings, 
+          headers = http_headers )  
+        request.close() 
+        last_update = time.ticks_ms()
